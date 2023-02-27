32cd0844f4a66ae4da7200c0a3570dd3c66b31df
Emil Sabri <emil.sabri@gmail.com>
2023-02-27 11:27:20 -0800

    asdf

--- middleware.js
+++ middleware.js
@@ -6,13 +6,13 @@ export default function middleware(request) {
     let url = new URL(request.url);
     let pathname = url.pathname;
     if (pathname === '/api') {
-        return new rewrite(new URL('/api', request.url))
+        return new rewrite(new URL('/blocked', request.url))
     }
 
     console.log('url', request.url)
     console.log('pathname', pathname)
 
     if (pathname === '/') {
-        return rewrite(new URL('/allow', 'https://multi-domains.vercel.app'))
+        return rewrite(new URL('/allow', request.url))
     }
 }
